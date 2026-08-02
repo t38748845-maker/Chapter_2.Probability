440d7e46b21f12214c698052810bfa772dec5cf2
t38748845-maker <t38748845@gmail.com>
2026-08-02 19:27:31 +0530

    Update Bayes'_Theorem_for_probability.py

--- Bayes'_Theorem_for_probability.py
+++ Bayes'_Theorem_for_probability.py
@@ -1,1 +1,23 @@
-#
+# Bayes' Theorem...
+# values of probability...
+p_coffee_drinker = 0.65
+p_cancer = 0.005
+p_coffee_drinker_given_cancer = 0.85
+
+# define formula...
+p_cancer_given_coffee_drinker = (p_coffee_drinker_given_cancer * p_cancer) / p_coffee_drinker
+
+# print p_cancer_given_coffee_drinker
+print(p_cancer_given_coffee_drinker)
+
+# values for probability...
+p_math_imaginary_marks = 30
+p_math_teacher_given_marks = 20
+p_math_exam_total_marks = 40
+
+# define formula...
+p_math_obtained_marks = (p_math_exam_total_marks * p_math_teacher_given_marks ) / p_math_imaginary_marks
+
+# print p_math_obtained_marks
+print(p_math_obtained_marks)
+
